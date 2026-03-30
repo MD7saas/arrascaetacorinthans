69863ac203ffdde5ef36fb009bffeb49f961637d
MD7saas <146314129+MD7saas@users.noreply.github.com>
2026-03-30 20:50:01 -0300

    Update atividade 3 - Joana.py

--- atividade 3 - Joana.py
+++ atividade 3 - Joana.py
@@ -1,4 +1,4 @@
-print("Essa é sua calculadora de porcentagem para seu produto!")
+print("vamos calcular porcentagem")
 produto = (float(input("Digite qual o valor do seu produto: ")))    
 porc = (float(input("Digite quantos % seria a porcentagem encima do produto (utilizando 1.01 para 1%)")))
 sfinal = produto * porc
