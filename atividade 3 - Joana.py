e60fbada7f5706ea525831b5a352f4c88e9e3d4a
MD7saas <146314129+MD7saas@users.noreply.github.com>
2026-03-30 20:50:30 -0300

    Update atividade 3 - Joana.py

--- atividade 3 - Joana.py
+++ atividade 3 - Joana.py
@@ -1,5 +1,5 @@
 print("vamos calcular porcentagem")
 produto = (float(input("Digite qual o valor do seu produto: ")))    
 porc = (float(input("Digite quantos % seria a porcentagem encima do produto (utilizando 1.01 para 1%)")))
-sfinal = produto * porc
-print (f"o valor total do produto é {sfinal} ")
+valorfinal = produto * porc
+print (f"o valor total do produto é {valorfinal} ")
